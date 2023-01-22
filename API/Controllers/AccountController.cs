2acf94f5df65b4fd8c0805013f4fa6b4bfca2538
Masoud Torkian <giyumtell.spectre@gmail.com>
2023-01-21 19:06:56 -0500

    login/register

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,7 +4,6 @@ using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Client;
 using System.Security.Cryptography;
 using System.Text;
 
